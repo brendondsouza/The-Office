c9dc469c4b4da805c16923b123f1c70180f4ab9d
Brendon DSouza <brendonmufc@gmail.com>
2022-05-06 11:56:02 -0400

    added event listners and images

--- js/main.js
+++ js/main.js
@@ -1,38 +1,59 @@
+//ADD EVENT LISTENER TO GET RANDOM QUOTE BUTTON
+const rando = document.querySelector('.random').addEventListener('click', getRandom)
 
-//document.querySelector('select').addEventListener('change', getFetch)
+//ADD EVENT LISTENER TO GET INDIVIDUAL CHARACTER QUOTE
+const choice = document.querySelector('.character-box')
+choice.addEventListener('click', event => {
+  const {target} = event
+  const {value} = target
+  console.log(event)
+  console.log(value)
+  if(!target.matches('button')){
+    return;
+  } else {
+    getFetch(value)
+  }
 
-const url = 'https://api.codetabs.com/v1/proxy?quest=https://officeapi.dev/api/characters'
+})
 
- //const character =  document.querySelector('select').value
-  //const url = 'https://api.codetabs.com/v1/proxy?quest=https://officeapi.dev/api/characters/' + character
+const url = 'https://api.codetabs.com/v1/proxy?quest=https://officeapi.dev/api/quotes'
 
+//IF CHARACTER IS CHOSEN RUN THIS FETCH FUNTION
+function getFetch(value){
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
         console.log(data.data)
-
-
-        let arr = []
-        data.data.forEach(item=> arr.push(`${item.firstname} ${item.lastname}-${item._id}`))
-
-        // for(let i = 0; i<data.data.length; i++){
-        //   arr[i] = `${data.data[i].firstname} ${data.data[i].lastname} ${data.data[i]._id}`
-        // } 
-        arr.map(i => i.split('-'))
-        console.log(arr);
-       
-        // // let id = data.
-        //   const obj = data.firstname; //turn message into an object 
-        //   const charcterArray = Object.keys(obj.firstname) //turn object into array of keys 
-        //   console.log(charcterArray);
-        //   // for(let i = 0; i < characterArray.length; i++){
-        //   //   const option = document.createElement('option') // create option
-        //   //   option.value = charArray[i] //give option a value 
-        //   //   option.innerText = charArray[i] // addinner text to each value 
-        //   //   select.appendChild(option) // append option to the select dropdown 
+        let arr = data.data.filter(x=> x.character._id === value)
+        console.log(arr) //[Math.floor(Math.random()*arr.length)])
+        console.log(arr[Math.floor(Math.random()*arr.length)])
+        const quote = arr[Math.floor(Math.random()*arr.length)]
+        document.querySelector('.insertQuote').innerText = quote.content
+        document.querySelector('.author').innerText = quote.character.firstname + ' ' + quote.character.lastname
       })
       .catch(err => {
           console.log(`error ${err}`)
       });
 
+    }
 
+    //IF RANDOM BUTTON IS CHOSEN RUN THIS FETCH
+    function getRandom(){
+      fetch(url)
+      .then(res => res.json()) // parse response as JSON
+      .then(data => {
+        console.log(data.data)
+        let arr = data.data
+        //let arr = data.data.filter(x=> x.character._id === value)
+        //console.log(arr) //[Math.floor(Math.random()*arr.length)])
+        console.log(data.data[Math.floor(Math.random()*arr.length)])
+        const quote = arr[Math.floor(Math.random()*arr.length)]
+        document.querySelector('.insertQuote').innerText = quote.content
+        document.querySelector('.author').innerText = quote.character.firstname + ' ' + quote.character.lastname
+
+      })
+      .catch(err => {
+          console.log(`error ${err}`)
+      });
+
+    }
